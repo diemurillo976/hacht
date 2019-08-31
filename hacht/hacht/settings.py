4da5a82c8ee0860545d5f1cf3085bf82dfac4c34
Martinvc96 <martinvc_96@hotmail.com>
2019-08-31 10:58:40 -0600

    update DB

--- hacht/hacht/settings.py
+++ hacht/hacht/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'hacht.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': 'hacht_DB',
+        'HOST': 'mongodb+srv://admin:admin@maincluster-rzyyj.mongodb.net/test?retryWrites=true&w=majority',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
     }
 }
 

98e824eabdcced847c6cd8479aabcbf846dc3617
Gerardo Monge <g.m.c_242@hotmail.com>
2019-09-03 22:01:20 -0600

    Prueba para input email

--- hacht/main/forms.py
+++ hacht/main/forms.py
@@ -7,7 +7,7 @@ roles= [
 
 class RegistrationForm(forms.Form):
     nombre = forms.CharField(max_length=40, widget=forms.TextInput(attrs={'class' : 'form-control item' }))
-    correo = forms.EmailField(max_length=40, widget=forms.TextInput(attrs={'class' : 'form-control item', 'type' : 'email' }))
-    password = forms.CharField(max_length=40, widget=forms.TextInput(attrs={'class' : 'form-control item',  'type' : 'password' }))
+    correo = forms.EmailField(max_length=40, widget=forms.TextInput(attrs={'class' : 'form-control item', 'type' : 'email', 'id' : 'email'}))
+    password = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class' : 'form-control item',  'type' : 'password' }))
     org = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class' : 'form-control item' }))
     rol = forms.CharField(widget=forms.Select(attrs={'class': 'btn btn-primary dropdown-toggle', 'data-toggle' : 'dropdown', 'aria-expanded' : 'false', 'type' : 'button', 'style' : 'height: 37px;'}, choices=roles))

645230ab81640ae865661a788f0aa69c424dbfad
Martinvc96 <martinvc_96@hotmail.com>
2019-09-13 22:29:27 -0600

    update

--- hacht/main/views.py
+++ hacht/main/views.py
@@ -56,4 +56,4 @@ def contact_us(request):
     return render(request, 'index/contact-us.html')
 
 def features(request):
-    return render(request, 'index/features.html')
+    return render(request, 'index/features.html' )

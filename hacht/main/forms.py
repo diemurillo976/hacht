d243433b7e83146c58acc40544a3469fdc8c6021
Martinvc96 <martinvc_96@hotmail.com>
2019-09-19 14:10:17 -0600

    update demo

--- hacht/main/forms.py
+++ hacht/main/forms.py
@@ -53,6 +53,7 @@ class Data_PacienteN(forms.ModelForm):
             "res" : forms.TextInput(attrs={'class':'form-control', 'type':'text'}), 
             "edad" : forms.TextInput(attrs={'class':'form-control', 'type':'text'}), 
             "sexo" : forms.Select(attrs={'class': 'btn btn-primary dropdown-toggle', 'data-toggle' : 'dropdown', 'aria-expanded' : 'false', 'type' : 'button', 'style' : 'height: 37px;'}, choices=sexo)
+        }
         
 class Data_Comp_Sesion(forms.Form):
 

bd02a808338579af82fe4eb7ba3f2b45eb35708e
Martinvc96 <martinvc_96@hotmail.com>
2019-09-20 23:47:20 -0600

    update

--- hacht/main/views.py
+++ hacht/main/views.py
@@ -88,7 +88,7 @@ def demo(request):
         result = forward_single_img(img)
         estimations = ["Adenosis", "Fibroadenoma", "Phyllodes Tumour", "Tubular Adenon", "Carcinoma", "Lobular Carcinoma", "Mucinous Carcinoma", "Papillary Carcinoma"]
         context = {"result": estimations[result],
-                   "irl": url
+                   "irl": str(url)
                    }
         return render(request, 'index/demo.html', context)
 

ae8efd738a5cac87b73b5db9926a0c0f0996b3e5
diemurillo976 <47539487+diemurillo976@users.noreply.github.com>
2020-06-08 19:43:33 -0600

    Revert "merge de branches individuales"

--- hacht/main/static/index/assets/js/header.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    $('#header_nav_elem a').each(function(){
-        if ($(this).prop('href') == window.location.href){
-            $(this).addClass('active');
-        }
-    });
-});
